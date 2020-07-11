a18bf35749f8bdb06bee539ae6cfe6500ccccd1b
elliotyagami <eatdmaxe@gmail.com>
2020-07-11 08:03:00 +0530

    Add matching card logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const hasClass = (elem, name) => {
+	let e = elem.className;
+	return e.indexOf(name) > -1 ? true : false;
+};
+
+function addClass(elem, cls) {
+	elem.className += ' ' + cls;
+}
+
+const attr = (elem, name) => {
+	return elem.getAttribute(name);
+};
+
+function removeClass(ele, cls) {
+	if (hasClass(ele, cls)) {
+		var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+		ele.className = ele.className.replace(reg, ' ');
+	}
+}
+
+function fadeOut(fadeTarget, interval) {
+	var fadeEffect = setInterval(function () {
+		if (!fadeTarget.style.opacity) {
+			fadeTarget.style.opacity = 1;
+		}
+		if (fadeTarget.style.opacity > 0) {
+			fadeTarget.style.opacity -= 0.1;
+		} else {
+			clearInterval(fadeEffect);
+		}
+	}, interval);
+}
+function fadeIn(fadeTarget, interval) {
+	var fadeEffect = setInterval(function () {
+		if (!fadeTarget.style.opacity) {
+			fadeTarget.style.opacity = 0;
+		}
+		if (fadeTarget.style.opacity > 0) {
+			fadeTarget.style.opacity += 0.1;
+		} else {
+			clearInterval(fadeEffect);
+		}
+	}, interval);
+}
+
 class App extends Component {
 	constructor() {
 		super();
@@ -69,8 +114,12 @@ class App extends Component {
 		this.state = {
 			cards: cds,
 			shuffleCards: this.shuffle(cds.concat(cds)),
+			paused: false,
+			guess: null,
 		};
 		this.renderCards = this.renderCards.bind(this);
+		this.checkCard = this.checkCard.bind(this);
+		this.win = this.win.bind(this);
 	}
 	shuffle(array) {
 		var counter = array.length,
@@ -89,21 +138,83 @@ class App extends Component {
 		}
 		return array;
 	}
+	checkCard(e) {
+		let elem = e.currentTarget;
+		// let inside= e.currentTarget.querySelector('inside')
+
+		if (
+			!this.state.paused &&
+			!hasClass(elem, 'matched') &&
+			!hasClass(elem, 'picked')
+		) {
+			addClass(elem, 'picked');
+			if (!this.state.guess) {
+				this.setState({ guess: attr(elem, 'data-id') });
+			} else if (
+				this.state.guess == attr(elem, 'data-id')
+				// !hasClass(elem, 'picked')
+			) {
+				Array.from(document.getElementsByClassName('picked')).forEach(
+					(ele) => {
+						addClass(ele, 'matched');
+					},
+				);
+				this.setState({ guess: null });
+			} else {
+				this.setState({ guess: null, pause: true });
+				let _this = this;
+				setTimeout(() => {
+					Array.from(
+						document.getElementsByClassName('picked'),
+					).forEach((ele) => {
+						removeClass(ele, 'picked');
+					});
+					_this.setState({ paused: false });
+				}, 500);
+			}
+			if (
+				document.getElementsByClassName('matched').length ==
+				this.state.shuffleCards.length
+			) {
+				this.win();
+			}
+		}
+	}
+	win() {
+		this.setState({ pause: true });
+		setTimeout(() => {
+			this.showModal();
+			fadeOut(document.getElementsByClassName('game')[0], 100);
+		}, 1000);
+	}
+	showModal() {
+		document.getElementsByClassName('modal-overlay').style.display =
+			'block';
+		fadeIn(document.getElementsByClassName('modal')[0], 100);
+	}
+	hideModal() {
+		document.getElementsByClassName('modal').style.display = 'none';
+		document.getElementsByClassName('modal-overlay').style.display = 'none';
+	}
 	renderCards() {
 		return (
 			<div className="game">
 				{this.state.shuffleCards.map((v, k) => {
 					return (
-						<div className="card" data-id={v.id} key={k}>
-							<div className="inside">
+						<div className="card" key={k}>
+							<div
+								className="inside"
+								data-id={v.id}
+								onClick={this.checkCard}
+							>
 								<div className="front">
 									<img src={v.img} alt={v.name} />
-									<div className="back">
-										<img
-											src="logos/arweave.png"
-											alt="arweave"
-										/>
-									</div>
+								</div>
+								<div className="back">
+									<img
+										src="logos/arweave.png"
+										alt="arweave"
+									/>
 								</div>
 							</div>
 						</div>
